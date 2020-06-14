5cfe43a9b8982abc7bbefbc1778ab557664344fa
lwh <1176465369@qq.com>
2020-06-14 11:26:32 +0800

    1

--- bkc/c21-send-op-xx/BLC/Accounts.go
+++ bkc/c21-send-op-xx/BLC/Accounts.go
@@ -1,40 +1,42 @@
 package BLC
 
-import(
-	"fmt"
-	"blcok"
-	"crypto/sha256"
+import (
+	//	"fmt"
+	//	"crypto/sha256"
+	//	"strconv"
 	"time"
 )
 
-type Account struct{
-	Name string
+type Account struct {
+	Name    string
 	Balance int
 	Address string
-	Days int
+	Days    int
 }
-var AccountsPool[] Account   //账户池
-var P_AccountsPool[] Account  //概率账户池
-var V_AccountsPool[] Account //竞选节点池
-var S_AccountsPool[] Account   //超级节点
+
+var AccountsPool []Account   //账户池
+var P_AccountsPool []Account //概率账户池
+var V_AccountsPool []Account //竞选节点池
+var S_AccountsPool []Account //超级节点
 
 //币龄随时间增加
-func Increasecoinage()  {
-	t := time.NewTicker(3*time.Second)
-	for {	
-		<-t.C:
-		for _, v := range AccountsPool{
-			AccountsPool[v].days++
-			}
+func Increasecoinage() {
+	t := time.NewTicker(3 * time.Second)
+	for {
+		<-t.C
+		for _, v := range AccountsPool {
+			v.Days++
 		}
+	}
 }
+
 //添加账户
-func AddNewAccount(name string，balance float32)Account{
+func AddNewAccount(name string, balance int) Account {
 	account := Account{
-		Name:name,
-		Balance:balance,
-		Address:strconv.Itoa(name + strconv.Itoa(balance)),
-		Days:0,
+		Name:    name,
+		Balance: balance,
+		Address: name,
+		Days:    0,
 	}
 	return account
 }
